b81b03e42859e6578788eb1f8c4a4bca70840ddc
Jeroen B <jeroenb04@live.com>
2022-10-16 14:35:03 +0200

    Create about page & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'about', to: 'static_pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
